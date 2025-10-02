97ba8bd0a710641af8b085139938171ca6142273
Lucccyo <cha.git@mailo.fr>
2025-10-02 14:10:29 +0200

    Add find identifier decl command

--- lua/ocaml/init.lua
+++ lua/ocaml/init.lua
@@ -172,6 +172,41 @@ function ocaml.find_identifier_def(identifier)
   end)
 end
 
+function ocaml.find_identifier_decl(identifier)
+  with_server(function(client)
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local params = {
+      uri = vim.uri_from_bufnr(0),
+      command = "locate",
+      args = {
+        "-position",
+        row + 1 .. ":" .. col,
+        "-prefix",
+        identifier,
+        "-look-for",
+        "interface",
+      },
+      resultAsSexp = false,
+    }
+    local result = client.request_sync("ocamllsp/merlinCallCompatible", params, 1000)
+    if not (result and result.result) then
+      vim.notify("No declaration for identifier " .. identifier .. ".", vim.log.levels.WARN)
+      return
+    end
+    local data = result.result.result
+    local ok, parsed = pcall(vim.fn.json_decode, data)
+    if not ok or not parsed.value or not parsed.value.pos then
+      vim.notify("Invalid response from server.", vim.log.levels.ERROR)
+      return
+    end
+    local file = parsed.value.file
+    local line = parsed.value.pos.line
+    local col = parsed.value.pos.col
+    vim.cmd.split(file)
+    vim.api.nvim_win_set_cursor(0, { line, col })
+  end)
+end
+
 --- Initialize the OCaml plugin
 ---@param config any
 function ocaml.setup(config)
@@ -208,8 +243,11 @@ function ocaml.setup(config)
       vim.api.nvim_create_user_command("FindIdentifierDefinition", function(opts)
         require("ocaml").find_identifier_def(opts.args)
       end, { nargs = 1 })
+
+      vim.api.nvim_create_user_command("FindIdentifierDeclaration", function(opts)
+        require("ocaml").find_identifier_decl(opts.args)
+      end, { nargs = 1 })
     end,
   })
 end
-
 return ocaml
