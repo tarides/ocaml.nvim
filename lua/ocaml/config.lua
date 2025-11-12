89dd519621face8a9ba1f39e11964dd0a77a6d07
Etienne Marais <self@maiste.fr>
2025-11-12 22:22:00 +0100

    feat: introduce a config.lua file
    Signed-off-by: Etienne Marais <self@maiste.fr>

---
+++ lua/ocaml/config.lua
@@ -0,0 +1,48 @@
+local vim = vim
+local M = {}
+
+---@class OCamlConfigKeymaps
+---@field jump_next_hole string|?
+---@field jump_prev_hole string|?
+---@field construct string|?
+---@field jump string|?
+---@field phrase_prev string|?
+---@field phrase_next string|?
+---@field infer string|?
+---@field switch_ml_mli string|?
+---@field type_enclosing string|?
+---@field type_enclosing_grow string|?
+---@field type_enclosing_shrink string|?
+---@field type_enclosing_increase string|?
+---@field type_enclosing_decrease string|?
+
+---@class ocaml.config.OCamlConfig
+---@field keymaps OCamlConfigKeymaps
+
+--- Default values
+--- @type ocaml.config.OCamlConfig
+local default = {
+  keymaps = {
+    jump_next_hole = "<leader>n",
+    jump_prev_hole = "<leader>p",
+    construct = "<leader>c",
+    jump = "<leader>j",
+    phrase_prev = "<leader>pp",
+    phrase_next = "<leader>pn",
+    infer = "<leader>i",
+    switch_ml_mli = "<leader>s",
+    type_enclosing = "<leader>t",
+    type_enclosing_grow = "<Up>",
+    type_enclosing_shrink = "<Down>",
+    type_enclosing_increase = "<Right>",
+    type_enclosing_decrease = "<Left>",
+  },
+}
+
+--- Extends the configuration with user config
+---@return ocaml.config.OCamlConfig
+function M.init_config(user_config)
+  return vim.tbl_deep_extend("force", default, user_config or {})
+end
+
+return M
